9a5c97baff4162f1fd003952788b65c334e63a82
Monica Bob <monicamihaela.bob@gmail.com>
2023-12-11 19:11:42 +0200

    update 11.12

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function showSkills() {
+    var oldPage = document.getElementById(projects)
+    oldPage.style.d = "none"
+    
+var page = document.getElementById("skills");
+page.style.display = "block"
+}
+function showLanguages() {
+    var oldPage = document.getElementById(projects)
+    oldPage.style.d = "none"
+    
+var page = document.getElementById("languages");
+page.style.display = "block"
+}
